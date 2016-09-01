238ef8f7ae127f59d15e5825f18f89f5d11c17c4
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2016-09-02 00:00:49 +0200

    Refined desktop mode menu and added mobile menu with scrolling close

--- js/app.js
+++ js/app.js
@@ -1,18 +1,11 @@
-// $(document).foundation()
-
-// Building menu
-var menu = document.querySelector('.menu');
-var bar = document.querySelector('.menu + .bar');
-bar.style.width = menu.querySelector('span').offsetWidth + 'px';
-menu.querySelectorAll('span').forEach( menuItem => {
-  menuItem.addEventListener('click', menuItemClick);
+// Media rules
+var mobileDevice = true;
+document.addEventListener("DOMContentLoaded", function() {
+  if( parseInt(window.innerWidth) > 767 ) mobileDevice = false;
+  
+  initViews();
 });
 
-function menuItemClick(e){
-  if( e.target.id == 'home' ){
-    bar.style.transform = 'translateX(0)';
-  } else {
-    var headerLeft = parseInt(window.getComputedStyle(document.querySelector('header')).marginLeft);
-    bar.style.transform = 'translateX('+ (e.target.offsetLeft - headerLeft) +'px)';
-  }
+function initViews(){
+ initMenu(); 
 }
---
+++ js/views.js
@@ -0,0 +1,70 @@
+/* Menu */
+function initMenu(){
+  if( mobileDevice )
+    initMobileMenu();
+  else
+    initDesktopMenu();
+}
+
+function initDesktopMenu(){
+  var titles = document.querySelector('.desktop .titles');
+  var bar = document.querySelector('.desktop .titles + .bar');
+  bar.style.width = titles.querySelector('span').offsetWidth + 'px';
+  titles.querySelectorAll('span').forEach( titlesItem => {
+    titlesItem.addEventListener('click', e => {
+      if( e.target.id == 'home' ){
+        bar.style.transform = 'translateX(0)';
+      } else {
+        var headerLeft = 
+          parseInt(window.getComputedStyle(document.querySelector('.desktop .menu')).marginLeft);
+        bar.style.transform = 'translateX('+ (e.target.offsetLeft - headerLeft) +'px)';
+      }
+    });
+  });
+}
+
+function initMobileMenu(){
+  var menuIcon = document.querySelector('#menu-icon');
+  var menu = document.querySelector('.mobile .menu');
+  menuIcon.addEventListener('click', e => {
+    if( menu.classList.contains('opened') )
+      menu.classList.remove('opened');
+    else
+      menu.classList.add('opened');
+  });
+  document.querySelector('body').addEventListener('click', e => {
+    if( !checkHit(e, menu) && !checkHit(e, menuIcon) )
+      menu.classList.remove('opened');
+  });
+  var startX, deltaX;
+  document.querySelector('body').addEventListener('touchstart', e => {
+    startX = parseInt( e.touches[0].pageX );
+  });
+  document.querySelector('body').addEventListener('touchmove', e => {
+    if( !menu.classList.contains('opened') ) return;
+    deltaX = parseInt( e.touches[0].pageX ) - startX;
+    if( deltaX >= 0 ) return;
+    menu.style.transform = 'translateX('+ deltaX +'px)';
+  });
+  document.querySelector('body').addEventListener('touchend', e => {
+    if( !menu.classList.contains('opened') ) return;
+    menu.style.transform = '';
+    if( deltaX < -150 )
+      menu.classList.remove('opened');
+  });
+  menu.querySelectorAll('.titles span').forEach( title => {
+    title.addEventListener('click', e => {
+      if( e.target.id == 'home' ) console.log('Home');
+      else if( e.target.id == 'ethos' ) console.log('Ethos');
+      else if( e.target.id == 'works' ) console.log('Works');
+    });
+  });
+}
+
+function checkHit(event, target){
+  for( var element = event.target; element.tagName != 'HTML'; element = element.parentNode ){
+    if( element == target )
+      return true;
+  }
+  return false;
+}
