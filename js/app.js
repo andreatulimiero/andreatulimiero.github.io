d766f7dafb138c1d5bb21260e464cf26834e5350
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2016-09-01 02:35:18 +0200

    Added toolbar with underline bar scrolling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-$(document).foundation()
+// $(document).foundation()
+
+// Building menu
+var menu = document.querySelector('.menu');
+var bar = document.querySelector('.menu + .bar');
+bar.style.width = menu.querySelector('span').offsetWidth + 'px';
+menu.querySelectorAll('span').forEach( menuItem => {
+  menuItem.addEventListener('click', menuItemClick);
+});
+
+function menuItemClick(e){
+  if( e.target.id == 'home' ){
+    bar.style.transform = 'translateX(0)';
+  } else {
+    var headerLeft = parseInt(window.getComputedStyle(document.querySelector('header')).marginLeft);
+    bar.style.transform = 'translateX('+ (e.target.offsetLeft - headerLeft) +'px)';
+  }
+}
