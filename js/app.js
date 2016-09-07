35aa0d27c79b6b1b2eba3938519f7602635c38f8
Tuly- <tulimiero.andrea@gmail.com>
2016-09-07 10:55:32 +0200

    Added analytics to the site

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ var mobileDevice = true;
 document.addEventListener("DOMContentLoaded", function() {
   if( parseInt(window.innerWidth) > 767 ) mobileDevice = false;
   
+  loadPolyfills();
+  registerAnalytics();
+
   initViews();
 });
 
@@ -10,4 +13,18 @@ function initViews(){
  initMenu(); 
 
  initWorks();
+}
+
+function registerAnalytics(){
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-83819121-1', 'auto');
+  ga('send', 'pageview');
+}
+
+function loadPolyfills(){
+  
 }
